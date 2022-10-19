7484bd8c963721d8753bc0bcc496e7c3df193504
KevinAS28 <kevinagusto28@gmail.com>
2022-10-19 09:40:27 +0700

    disable file dirs check

--- file_server0/views.py
+++ file_server0/views.py
@@ -8,36 +8,40 @@ files_dirs = json.load(open(os.path.join(settings.BASE_DIR, 'files_dirs.json')))
 
 # Create your views here.
 def download(request, filename):
+    print('requested file: ' + filename)
     for file_path in files_dirs['files']:
         file = os.path.basename(file_path)
-        if os.path.isfile(file):
+        # if os.path.isfile(file):
+        if True:
             if file==filename:
                 with open(file_path, 'rb') as file_fd:
                     response = HttpResponse(file_fd.read(), content_type='application/octet-stream')
                     response['Content-Disposition'] = 'inline; filename='+file
                     return response
-            else:
-                print(f'file {file_path} not match')
+            # else:
+            #     print(f'file {file_path} not match')
         else:
             print(f'file {file_path} not exist!')
 
     for dir_path in files_dirs['dirs']:
-        if os.path.isdir(dir_path):
+        # if os.path.isdir(dir_path):
+        if True:
             for root, dirs, files in os.walk(dir_path, topdown=False):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    if os.path.isfile(file):
+                    # if os.path.isfile(file):
+                    if True:
                         if file==filename:
                             with open(file_path, 'rb') as file_fd:
                                 response = HttpResponse(file_fd.read(), content_type='application/octet-stream')
                                 response['Content-Disposition'] = 'inline; filename='+file
                                 return response     
-                        else:
-                            print(f'file {file_path} not match!')
+                        # else:
+                        #     print(f'file {file_path} not match!')
                     else:
                         print(f'file {file_path} not exist!')          
-        else:
-            print(f'dir {dir_path} not exist!')     
+        # else:
+        #     print(f'dir {dir_path} not exist!')     
 
             
     return HttpResponseNotFound(f'file {filename} not exist!')
